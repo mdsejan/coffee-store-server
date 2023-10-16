8c07a0d1ae05a95fe17e842394fd4f8d53dde39b
Sejan~ <md4sejan@gmail.com>
2023-10-17 02:41:28 +0600

    update data using put method

--- index.js
+++ index.js
@@ -37,10 +37,39 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeCollection.findOne(query);
+            res.send(result)
+        })
+
         app.post('/coffees', async (req, res) => {
             const newCoffee = req.body;
             const result = await coffeCollection.insertOne(newCoffee);
             res.send(result);
+        })
+
+        app.put('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedCoffee = req.body;
+
+            const coffee = {
+                $set: {
+                    name: updatedCoffee.name,
+                    chef: updatedCoffee.chef,
+                    supplier: updatedCoffee.supplier,
+                    taste: updatedCoffee.taste,
+                    category: updatedCoffee.category,
+                    details: updatedCoffee.details,
+                    photo: updatedCoffee.photo
+                }
+            }
+
+            const result = await coffeCollection.updateOne(filter, coffee, options)
+            res.send(result)
         })
 
         app.delete('/coffees/:id', async (req, res) => {
